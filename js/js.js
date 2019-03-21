9603e8f82692ccb79323c9e89dd292a76ab56e28
Gleb Dolgushin <dolgushing@gmail.com>
2019-03-21 06:30:59 +0300

    правки
    правки

--- js/js.js
+++ js/js.js
@@ -45,13 +45,45 @@ ThirdSlideBtn.addEventListener("click", function (evt) {
     FirstSlideBtn.classList.remove("slider-btn--checked");
     SecondSlideBtn.classList.remove("slider-btn--checked");
     ThirdSlideBtn.classList.add("slider-btn--checked");
-    Container.classList.remove("container--second-slide");
-    Container.classList.remove("container--first-slide");
-    Container.classList.add("container--third-slide");
+    Container.classList.remove("slider--second-slide");
+    Container.classList.remove("slider--first-slide");
+    Container.classList.add("slider--third-slide");
     Body.classList.remove("page-body--green");
     Body.classList.remove("page-body--blue");
     Body.classList.add("page-body--brown");
     FirstSliderText.classList.add("slider__text--hide");
     SecondSliderText.classList.add("slider__text--hide");
     ThirdSliderText.classList.remove("slider__text--hide");
+});
+
+
+
+var link = document.querySelector(".contacts__button"),
+    popup = document.querySelector(".modal-write-us"),
+    close = popup.querySelector(".modal-close"),
+    form = popup.querySelector("form"),
+    name1 = popup.querySelector(".write-us-form-name"),
+    email = popup.querySelector(".write-us-form-email"),
+    text = popup.querySelector(".write-us-form-letter-text");
+
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+});
+
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!name1.value || !email.value || !text.value) {
+        evt.preventDefault();
+        popup.classList.remove("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
+    }
 });
